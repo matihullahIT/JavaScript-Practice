0d07d57bf358b8f6a9157543156b73049dbe40ff
matihullahIT <matihurrehman.official@gmail.com>
2025-04-12 15:59:05 +0500

    Refactor index.html to remove calculator elements and update title; clean up index.js by removing commented-out code

--- calculator/index.js
+++ calculator/index.js
@@ -17,21 +17,3 @@ function Clear(){
     output.value = ""; 
     
 }
-
-// // let symbol, results = null;
-
-// function input(value) {
-//     var output = document.getElementById("output");
-//     output.value += value; // Append the button value to the output
-// }
-// function clear(){
-    
-// }
-// function calculate() {
-//     var output = document.getElementById("output");
-//     try {
-//         output.value = eval(output.value); // Evaluate the expression
-//     } catch (e) {
-//         output.value = "Error"; // Handle invalid expressions
-//     }
-// }

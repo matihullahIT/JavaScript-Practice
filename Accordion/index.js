1a4f2163cb97da57a4a24f6b24e94a7609452d05
matihullahIT <matihurrehman.official@gmail.com>
2025-04-22 01:14:58 +0500

    Implement image carousel functionality for banner with next/prev controls

--- Accordion/index.js
+++ Accordion/index.js
@@ -1,9 +1,33 @@
 $(document).ready(() => {
+    const banner = $("#banner")
+    const imageurls=["./media/Landscape_01 - Without Billing.png","./media/pxfuel (1).png","./media/Landscape_01 - Without Billing.png",
+        "./media/HD-wallpaper-suleymaniye-mosque-istanbul-cityscape-beautiful-city-bosphorus-turkey-mosque-ottoman-architecture.jpg",
+        "./media/Landscape_01 - Without Billing.png",
+        "./media/pxfuel (1).png"
+    ] 
+    
+let i = 0;
+            banner.css("background-image", `url("${imageurls[i]}")`);
+
+function next(){
+              banner.css("background-image", `url("${imageurls[i]}")`);
+              i = (i + 1) % imageurls.length; // Loop back to 0 after last item
+                  // setTimeout(3000);
+          }
+$("#next").click(next);
+$("#prev").click(() => {
+  i = (i - 1 + imageurls.length) % imageurls.length; // Ensure non-negative index
+  banner.css("background-image", `url("${imageurls[i]}")`);
+});
+
+    // updateBanner();
     $(".label").click(function () {
       const answer = $(this).next(".answer");
       $(".answer").not(answer).slideUp("slow"); 
       answer.slideToggle("slow"); 
     });
+
+
         $(".nav-elements").hover($("#nav-item-container").hover(
             function () {
               // On mouse enter, show the details
@@ -13,7 +37,6 @@ $(document).ready(() => {
               // On mouse leave, hide the details
               $(this).find(".details").stop(true, true).slideUp("fast");
             }
-          ));
-
-
+          ))
+            
   });
